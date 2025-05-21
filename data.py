7bb98678166230659c3a452f6e05395ea9a09b79
rasmine <blasrasmine@gmail.com>
2025-05-21 13:07:55 +0800

    add load_questions function with categorized questions

--- data.py
+++ data.py
@@ -1,3 +1,10 @@
 class DataManager:
     def __init__(self, filename="quiz_data.txt"):
         self.filename = filename
+    def load_questions(self):
+        questions_data = {
+            "Easy": {"Food and Drinks": [], "Science": [], "Geography": [], "Movies": [], "Pop Culture": [], "Literature": [], "History": []},
+            "Medium": {"Food and Drinks": [], "Science": [], "Geography": [], "Movies": [], "Pop Culture": [], "Literature": [], "History": []},
+            "Hard": {"Food and Drinks": [], "Science": [], "Geography": [], "Movies": [], "Pop Culture": [], "Literature": [], "History": []}
+        }
+        original_total_questions = 0
