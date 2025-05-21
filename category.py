49d04cbdc415464ffc6a14ab6858f40b2aeea1c7
rasmine <blasrasmine@gmail.com>
2025-05-21 14:29:56 +0800

    add display_randomizing_ui method to setup random category label and update loop

--- category.py
+++ category.py
@@ -11,6 +11,18 @@ class CategoryRandomizer:
 
         self.display_randomizing_ui()
     
+    def display_randomizing_ui(self):
+        self.clear_window()
+        self.window.config(bg="#FFD8B1")
+
+        self.category_box = tk.Label(self.window, text="...", font=("Comic Sans MS", 50, "bold"), fg="#F88379", bg="#FCE0D6",
+                                 width=20, height=2, relief="solid")
+        self.category_box.pack(pady=50)
+
+        self.update_category()
+        self.window.after(2000, self.finish_randomizing)
+
+    
     def clear_window(self):
         for widget in self.window.winfo_children():
             widget.destroy()
