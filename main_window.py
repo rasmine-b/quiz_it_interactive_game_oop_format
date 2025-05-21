e2f535436dfd983f5c00ebe926b334608b7177e9
rasmine <blasrasmine@gmail.com>
2025-05-21 14:16:00 +0800

    add MainWindow class with initializer

---
+++ main_window.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class MainWindow:
+    def __init__(self, window, on_start, on_exit, on_saved_quiz):
+        self.window = window
+        self.on_start = on_start
+        self.on_exit = on_exit
+        self.on_saved_quiz = on_saved_quiz
+        self.setup_main_screen()
