b7539301dace15596f45f8d6297644e07881ef3c
rasmine <blasrasmine@gmail.com>
2025-05-21 13:49:30 +0800

    add methods to get difficulties and categories from questions data

--- quiz.py
+++ quiz.py
@@ -4,3 +4,9 @@ class QuizLogic:
         self.score_correct = 0
         self.score_total = 0
 
+    def get_difficulties(self):
+        return list(self.questions_data.keys())
+    
+    def get_categories(self, difficulty):
+        return list(self.questions.get(difficulty, {}).keys())
+    
