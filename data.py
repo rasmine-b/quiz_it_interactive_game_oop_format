d7f76eb216b26d778b25dae43f1c5af3f776551a
rasmine <blasrasmine@gmail.com>
2025-05-21 13:12:50 +0800

    add file handling

--- data.py
+++ data.py
@@ -8,3 +8,8 @@ class DataManager:
             "Hard": {"Food and Drinks": [], "Science": [], "Geography": [], "Movies": [], "Pop Culture": [], "Literature": [], "History": []}
         }
         original_total_questions = 0
+        try:
+            with open(self.filename, "r") as file:
+                lines = file.readlines()
+        except FileNotFoundError:
+            lines = []
